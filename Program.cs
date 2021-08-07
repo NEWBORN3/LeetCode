c3c6a094a2d6acfe7f76ad70efb70d6b7d22789b
NewBorn <newborn3x@gmail.com>
2021-08-08 02:08:28 +0300

    Clean program.cs file

--- Program.cs
+++ Program.cs
@@ -7,10 +7,6 @@ namespace LeetCode
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int[] num3 = {0,1,2,2,3,0,4,2};
-            int val = 2;
-            int un = TheArray.RemoveElement(num3,val);
-            Console.WriteLine("o- {0}",un);        
         }
     }
 }
